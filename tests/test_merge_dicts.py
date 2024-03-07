9c9d3247c7d532a78aa1251a270e57c90dc0546d
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2024-03-07 17:44:58 -0500

    fix test function name

--- tests/test_merge_dicts.py
+++ tests/test_merge_dicts.py
@@ -6,7 +6,7 @@ sys.path.append(dirname)
 
 from neurocaps.analysis import merge_dicts
 
-def test_mege_dicts():
+def test_merge_dicts():
     subject_timeseries = {str(x) : {f"run-{y}": np.random.rand(100,400) for y in range(1,4)} for x in range(1,11)}
     subject_timeseries_combined = merge_dicts([subject_timeseries,subject_timeseries])
     assert subject_timeseries_combined["1"]["run-1"].shape == (200,400)
