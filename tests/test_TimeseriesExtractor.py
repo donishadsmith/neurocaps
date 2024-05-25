0fd4eb7fa0f65a35ca6c0e72434b810018808689
Donisha Smith <donishasmith@outlook.com>
2024-05-25 00:14:02 -0400

    Allow nilearn versions 0.10.1, 0.10.2, and 0.10.4
    Allow nilearn versions 0.10.1, 0.10.2, and 0.10.4
    
    test nilearn
    
    test nilearn
    
    test nilearn
    
    test nilearn
    
    test nilearn

--- tests/test_TimeseriesExtractor.py
+++ tests/test_TimeseriesExtractor.py
@@ -64,6 +64,27 @@ def test_TimeseriesExtractor_no_parallel_no_session_w_custom_and_censoring():
     assert extractor.subject_timeseries["01"]["run-001"].shape[-1] == 426
     assert extractor.subject_timeseries["01"]["run-001"].shape[0] == 39
 
+def test_TimeseriesExtractor_dummy():
+    dir = os.path.dirname(__file__)
+
+    confounds=["Cosine*", "aComp*", "Rot*"]
+
+    parcel_approach = {"Custom": {"maps": os.path.join(dir, "HCPex.nii.gz")}}
+
+    extractor = TimeseriesExtractor(parcel_approach=parcel_approach, standardize="zscore_sample",
+                                    use_confounds=True, detrend=True, low_pass=0.15, high_pass=0.01,
+                                    confound_names=confounds, dummy_scans=5)
+
+    bids_dir = os.path.join(dir, "ds000031_R1.0.4_ses001-022/ds000031_R1.0.4")
+
+    pipeline_name = "fmriprep_1.0.0/fmriprep"
+    extractor.get_bold(bids_dir=bids_dir, task="rest", pipeline_name=pipeline_name, tr=1.2)
+    
+    print(extractor.subject_timeseries, flush=True)
+
+    assert extractor.subject_timeseries["01"]["run-001"].shape[-1] == 426
+    assert extractor.subject_timeseries["01"]["run-001"].shape[0] == 35
+
 def test_TimeseriesExtractor_check_exclusion():
     dir = os.path.dirname(__file__)
 
