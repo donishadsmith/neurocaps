355afba90cfd66c50792645a90327a0623d3efad
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2024-11-18 03:36:25 -0500

    Minor code cleaning

--- neurocaps/_utils/extraction/check_confound_names.py
+++ neurocaps/_utils/extraction/check_confound_names.py
@@ -8,16 +8,41 @@ def _check_confound_names(high_pass, user_confounds, n_acompcor_separate):
         if high_pass:
             # Do not use cosine or acompcor regressor if high-pass filtering is not None.
             # Acompcor regressors are estimated on high pass filtered version of data form fmriprep
-            confound_names = ["trans_x", "trans_x_derivative1","trans_y", "trans_y_derivative1",
-                              "trans_z", "trans_z_derivative1",  "rot_x", "rot_x_derivative1",
-                              "rot_y", "rot_y_derivative1", "rot_z", "rot_z_derivative1"
+            confound_names = [
+                "trans_x",
+                "trans_x_derivative1",
+                "trans_y",
+                "trans_y_derivative1",
+                "trans_z",
+                "trans_z_derivative1",
+                "rot_x",
+                "rot_x_derivative1",
+                "rot_y",
+                "rot_y_derivative1",
+                "rot_z",
+                "rot_z_derivative1"
             ]
         else:
             confound_names = [
-                "cosine*","trans_x", "trans_x_derivative1","trans_y", "trans_y_derivative1",
-                "trans_z", "trans_z_derivative1",  "rot_x", "rot_x_derivative1",
-                "rot_y", "rot_y_derivative1", "rot_z", "rot_z_derivative1", "a_comp_cor_00",
-                "a_comp_cor_01", "a_comp_cor_02", "a_comp_cor_03", "a_comp_cor_04", "a_comp_cor_05"
+                "cosine*",
+                "trans_x",
+                "trans_x_derivative1",
+                "trans_y",
+                "trans_y_derivative1",
+                "trans_z",
+                "trans_z_derivative1",
+                "rot_x",
+                "rot_x_derivative1",
+                "rot_y",
+                "rot_y_derivative1",
+                "rot_z",
+                "rot_z_derivative1",
+                "a_comp_cor_00",
+                "a_comp_cor_01",
+                "a_comp_cor_02",
+                "a_comp_cor_03",
+                "a_comp_cor_04",
+                "a_comp_cor_05"
             ]
     else:
         assert isinstance(user_confounds, list) and user_confounds, "`confound_names` must be a non-empty list."
@@ -35,8 +60,8 @@ def _acompcor_check(confound_names, user_confounds, n):
         removed_confounds = [element for element in confound_names if element not in check_confounds]
         if user_confounds:
             LG.warning("Since `n_acompcor_separate` has been specified, acompcor components in "
-                        f"`confound_names` will be disregarded and replaced with the first {n} "
-                        "components of the white matter and cerebrospinal fluid masks for each participant. "
-                        f"The following components will not be used {removed_confounds}.")
+                       f"`confound_names` will be disregarded and replaced with the first {n} "
+                       "components of the white matter and cerebrospinal fluid masks for each participant. "
+                       f"The following components will not be used {removed_confounds}.")
 
     return check_confounds
