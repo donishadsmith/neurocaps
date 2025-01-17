664efee873dbd66eb62ae9c560e886834ef724ae
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2025-01-16 21:53:01 -0500

    [skip ci] Update Dockerfile

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -3,8 +3,10 @@
 # Off-screen rendering
 Xvfb :0 -screen 0 1600x1200x24 & export DISPLAY=:0
 
-# Sleep
-sleep 1
+# Variable wait time to ensure Xvfb start; send standard output and error to the void
+while !xset q &> /dev/null; do
+    sleep 0.1
+done
 
 if [ "$1" = "notebook" ]; then
     jupyter notebook --allow-root --no-browser --ip=0.0.0.0 --port=9999
