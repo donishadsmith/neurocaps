be6942b346b74f8b14d45e69a950ff13cf2edc9a
Donisha Smith <112973674+donishadsmith@users.noreply.github.com>
2025-07-12 03:50:05 -0400

    Get correct density

--- neurocaps/__init__.py
+++ neurocaps/__init__.py
@@ -4,4 +4,4 @@ from . import analysis, extraction, exceptions, utils
 __all__ = ["analysis", "extraction", "exceptions", "utils"]
 
 # Version in single place
-__version__ = "0.32.3"
+__version__ = "0.32.4"
