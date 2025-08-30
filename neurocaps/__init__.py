a750aa87d028b862b38536b95e7fb416031ad84b
Donisha Smith <donishasmith@outlook.com>
2025-08-29 22:41:11 -0400

    Add docs to init [skip ci]

--- neurocaps/__init__.py
+++ neurocaps/__init__.py
@@ -1,3 +1,21 @@
+"""
+Co-Activation Patterns (CAPs) for resting-state and task-based fMRI data.
+-------------------------------------------------------------------------
+Documentation and comprehensive tutorials can be found at https://neurocaps.readthedocs.io.
+
+Submodules
+---------
+extraction -- Contains ``TimeseriesExtractor`` class for timeseries extraction quality control, and BOLD visualization
+
+analysis -- Class and functions for co-activation patterns analysis, merging timeseries ROI standardization,
+and changing timeseries dtype
+
+typing -- Type definitions for ``SubjectTimeseries``, ``ParcelConfig``, and ``ParcelApproach``
+
+utils -- Tools for fetching preset parcellation approaches, creating parcellation approaches from
+tabular metadata, data simulation, and plotting defaults
+"""
+
 from . import analysis, extraction, exceptions, utils
 
 # Don't include "typing"
