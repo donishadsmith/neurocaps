60dedadba7900c90b40218a45b2748453215fccd
donishadsmith <donishasmith@outlook.com>
2025-11-06 12:08:25 -0500

    Minor doc update [skiip ci]

--- neurocaps/__init__.py
+++ neurocaps/__init__.py
@@ -4,7 +4,7 @@ Co-Activation Patterns (CAPs) for resting-state and task-based fMRI data.
 Documentation and comprehensive tutorials can be found at https://neurocaps.readthedocs.io.
 
 Submodules
----------
+----------
 extraction -- Contains ``TimeseriesExtractor`` class for timeseries extraction quality control, and BOLD visualization
 
 analysis -- Class and functions for co-activation patterns analysis, merging timeseries ROI standardization,
